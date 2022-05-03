acdc25c35dc0fb884389b5aef6966d66edf60b21
cnh574 <102606659+cnh574@users.noreply.github.com>
2022-05-03 08:57:51 -0400

    Add files via upload

---
+++ portfolio/app.js
@@ -0,0 +1,29 @@
+// $(() => {
+//   $("<div>").attr("id", "navbar").appendTo("#container");
+//   $("<div>").attr("id", "btn2").text("Resume").appendTo("#navbar");
+//   $("#btn2").on("click", () => {
+//     location.href = "/Portfolio/resume/index.html";
+//   });
+// });
+
+// $(() => {
+//   $("<div>").attr("id", "btn3").text("Bio").appendTo("#navbar");
+//   $("#btn3").on("click", () => {
+//     location.href = "/Portfolio/bio/index.html";
+//   });
+// });
+
+// $(() => {
+//   $("<div>").attr("id", "btn1").text("Projects").appendTo("#navbar");
+//   $("#btn1").on("click", () => {
+//     location.href ="Portfolio/project/index.html";
+//   });
+// });
+
+$(() => {
+  $("h1").on({
+    mouseenter: function () {
+      $("h1").css("color", "pink");
+    },
+  });
+});
---
+++ portfolio/bioapp.js
@@ -0,0 +1,16 @@
+$(() => {
+  $("#linkedin").on({
+    mouseenter: function () {
+      $("#linkedin").css("color", "Green");
+    },
+  });
+});
+
+$(() => {
+  $("#Github").on({
+    mouseenter: function () {
+      $("#Github").css("color", "Green");
+    },
+  });
+});
+
